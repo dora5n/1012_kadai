e16b8f0c7fb8d10eeda9794aa2dd892348b88656
dora5n <Moe4dora5n@gmail.com>
2023-10-23 15:12:42 +0900

    h1のcss修正

--- src/components/DoubleClick.jsx
+++ src/components/DoubleClick.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const DoubleClick = () => {
-  const [text, setText] = useState('よくある質問・お問い合わせ');
+  const [text, setText] = useState("よくある質問・お問い合わせ");
 
   const handleDoubleClick = () => {
     setText((prevText) =>
-      prevText === 'よくある質問・お問い合わせ' ? '新しいテキスト' : 'よくある質問・お問い合わせ'
+      prevText === "よくある質問・お問い合わせ"
+        ? "新しいテキスト"
+        : "よくある質問・お問い合わせ"
     );
   };
 
   return (
-    <h1 onDoubleClick={handleDoubleClick} style={{ marginLeft: '200px', color: 'white' }}>
+    <h1
+      onDoubleClick={handleDoubleClick}
+      style={{
+        marginLeft: "200px",
+        color: "white",
+        fontWeight: "bold",
+        fontSize: "40px",
+        width: "1200px",
+        margin: "40px auto",
+      }}
+    >
       {text}
     </h1>
   );
