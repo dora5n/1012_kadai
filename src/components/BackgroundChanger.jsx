368be8ef094182bc977fcde3b679847a032a746d
dora5n <Moe4dora5n@gmail.com>
2023-10-29 02:38:21 +0900

    スライドショー実装

--- src/components/BackgroundChanger.jsx
+++ src/components/BackgroundChanger.jsx
@@ -1,40 +1,52 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import '../styles/BackgroundChanger.css';
 
-const Index = () => {
-  const [backgroundStyle, setBackgroundStyle] = useState({
-    width: '1200px',
-    height: '660px',
-    background: 'url(img/arata.jpg) no-repeat center/cover',
-    margin: '50px auto',
-    borderRadius: '10px',
-  });
+const settings = {
+  dots: false,
+  infinite: true,
+  speed: 3000, // 2秒ごとのスピードに設定
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true, // 自動再生を有効化
+  autoplaySpeed: 3000, // 2秒ごとの自動再生速度
+  vertical: true, // 垂直方向にスライド
+};
+
+const BackgroundChanger = () => {
+  const defaultImages = [
+    "/img/arata.jpg",
+    "/img/kid.jpg",
+    "/img/seiko.jpg",
+    "/img/seiko2.jpg"
+  ];
+
+  const [images, setImages] = useState(defaultImages);
 
   useEffect(() => {
-    // locationからパラメーターを取得
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get('myParam');
-    // パラメーターがある場合、背景画像を変更
     if (myParam) {
-      setBackgroundStyle(prevStyle => ({
-        ...prevStyle,
-        background: `url(img/arata2.jpg) no-repeat center/cover`,
-      }));
+      setImages([
+        "/img/arata2.jpg",
+        "/img/kid.jpg",
+        "/img/seiko.jpg",
+        "/img/seiko2.jpg"
+      ]);
     }
   }, []);
 
   return (
-    <div
-      style={{
-        width: backgroundStyle.width,
-        height: backgroundStyle.height,
-        background: backgroundStyle.background,
-        margin: backgroundStyle.margin,
-        borderRadius: backgroundStyle.borderRadius,
-      }}
-    >
-      {/* ここにコンテンツを追加 */}
+    <div className="background-changer-container">
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index} className="background-slide">
+            <img src={image} alt={`slide ${index}`} className="background-image" />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
 
-export default Index;
+export default BackgroundChanger;
