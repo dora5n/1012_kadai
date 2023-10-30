e4514266acdf569ec88587c4ed9ed693cd2c8343
dora5n <Moe4dora5n@gmail.com>
2023-10-30 13:39:00 +0900

    スライドショーの修正

--- src/components/BackgroundChanger.jsx
+++ src/components/BackgroundChanger.jsx
@@ -2,12 +2,11 @@ import 'swiper/css';
 import 'swiper/css/bundle';
 import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import '../styles/styles.css';
 
 const BackgroundChanger = () => {
   const [backgroundStyle, setBackgroundStyle] = useState({
     width: '1200px',
-    height: '680px',
+    height: '660px',
     margin: '50px auto',
     borderRadius: '10px',
   });
@@ -36,20 +35,29 @@ const BackgroundChanger = () => {
         borderRadius: backgroundStyle.borderRadius,
       }}
       slidesPerView={1} // 1枚ずつ表示
-      direction={'horizontal'} // 水平方向にスライド
+      // direction={'horizontal'} // 水平方向にスライド
+      // autoplay={{ delay: 2000 }} // 自動再生
     >
       <SwiperSlide>
-        <img src="/img/arata.jpg" alt="arata" className="swiper-image" />
+        <img src="/img/arata.jpg" alt="arata" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="/img/kid.jpg" alt="kid" className="swiper-image" />
+        <img src="/img/kid.jpg" alt="kid" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="/img/seiko.jpg" alt="seiko" className="swiper-image" />
+        <img src="/img/seiko.jpg" alt="seiko" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="/img/seiko2.jpg" alt="seiko2" className="swiper-image" />
-      </SwiperSlide>    </Swiper>
+        <img src="/img/seiko2.jpg" alt="seiko2" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/img/karerin.jpg" alt="karerin" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/img/resu.jpg" alt="resu" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      </SwiperSlide>
+
+    </Swiper>
   );
 };
 
